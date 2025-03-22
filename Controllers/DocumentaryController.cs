218b9f811681d9dab13c3259bdbf3da593f4153d
Ben <bencross86@gmail.com>
2025-03-22 15:40:27 +0000

    Return single item by id

--- Controllers/DocumentaryController.cs
+++ Controllers/DocumentaryController.cs
@@ -12,17 +12,19 @@ public class DocumentaryController(DocumentaryContext context) : ControllerBase
     [HttpGet("{id}")]
     public async Task<IActionResult> Get(string id)
     {
-        Documentary documentary = new Documentary(id, "Super Doc", "Ben Cross");
-        
-        return await Task.FromResult(Ok(documentary));
+        var documentary = await context.Documentaries.FindAsync(id);
+
+        if (documentary == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(documentary);
     }
 
     [HttpGet]
     public async Task<IActionResult> List()
     {
-        // Documentary documentary = new Documentary("1111-2222-3333-4444", "Super Doc", "Ben Cross");
-        // List<Documentary> documentaries = new List<Documentary>([documentary]);
-
         var documentaries = await context.Documentaries.ToListAsync();
         
         return await Task.FromResult(Ok(documentaries));
